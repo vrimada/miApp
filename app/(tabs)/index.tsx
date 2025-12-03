234f5d16817141f39a0c94e009959ce07960ab65
Vanesa Rimada <80859025+vrimada@users.noreply.github.com>
2025-12-03 13:07:07 -0300

    feature: storage para los gastos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Device from 'expo-device';
 import * as Notifications from "expo-notifications";
-import { useEffect } from "react";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+
 // Esto hace que las notificaciones se muestren incluso con la app abierta
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -13,8 +15,37 @@ Notifications.setNotificationHandler({
   }),
 });
 
+type Servicio = {
+    id: string;
+    nombre: string;
+    fechaISO: string;
+    fechaDisplay: string;
+  };
+
 export default function Index() {
+  const [servicios, setServicios] = useState<Servicio[]>([]);
+
+   const cargarServicios = async () => {
+    try {
+      //Lee el almacenamiento
+      const data = await AsyncStorage.getItem("servicios");
+      //Lo convierte en un array
+       const lista: Servicio[] = data ? JSON.parse(data) : [];
+
+
+      // Ordena por fecha ISO (YYYY-MM-DD)
+      lista.sort((a: any, b: any) =>
+        a.fechaISO.localeCompare(b.fechaISO)
+      );
+
+      setServicios(lista);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
   useEffect(() => {
+      cargarServicios();
     // Pedir permisos apenas abre la app
      if (Device.isDevice) 
         Notifications.requestPermissionsAsync();
@@ -41,10 +72,35 @@ export default function Index() {
     >
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text style={{ fontSize: 20, color: '#fff' }}>Vencimientos pendientes</Text>
-      <Text style={{ marginTop: 10 ,color: '#fff' }}>
-        (Acá mostraremos los próximos vencimientos)
-      </Text>
+     
     </View>
+
+     {servicios.length === 0 ? (
+        <Text style={{ fontSize: 16 , color: '#fff' }}>No hay vencimientos cargados.</Text>
+      ) : (
+        <FlatList
+          data={servicios}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                padding: 12,
+                borderWidth: 1,
+                borderRadius: 8,
+                marginBottom: 10,
+              }}
+            >
+              <Text style={{ fontSize: 18, fontWeight: "600" }}>
+                {item.nombre}
+              </Text>
+              <Text style={{ fontSize: 16 }}>
+                Vence: {item.fechaDisplay}
+              </Text>
+            </View>
+          )}
+        />
+      )}
+      
       {/* <Button title="Enviar notificación" onPress={enviarNotificacion} />
       <Link href="/(tabs)/profile" style={{color: '#fff'}}> Ir a Gastos</Link> */}
     </View>
